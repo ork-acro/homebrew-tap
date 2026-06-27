2e98231536c8e9e917c8394aeed861ee940c7000
ork-acro <80279367+ork-acro@users.noreply.github.com>
2026-06-27 11:14:14 +0300

    local-forward 7.0.0

--- Casks/local-forward.rb
+++ Casks/local-forward.rb
@@ -1,6 +1,6 @@
 cask "local-forward" do
-  version "6.0.0"
-  sha256 "43014c77d50f6acdc69ed7865bd46e98c27e013c057604e541cd83a4b203291c"
+  version "7.0.0"
+  sha256 "f86911b97e603a7589bbcacc50678bdb518ed6aeb1f234839f70f2a386e2b238"
 
   url "https://github.com/ork-acro/local-forward-releases/releases/download/v#{version}/LocalForward-v#{version}.dmg"
   name "LocalForward"
